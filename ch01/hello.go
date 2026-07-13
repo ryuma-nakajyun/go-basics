528500289e61076f9cb4d79223558c8f04d6d37d
ryuma-nakajyun <ryuma.nakajyun@outlook.com>
2026-07-13 17:05:43 +0900

    update:file read function

--- ch01/hello.go
+++ ch01/hello.go
@@ -4,13 +4,28 @@ import (
 	"fmt"
 	"time"
 	"runtime"
+	"os"
+	"bufio"
 )
 
+func readFile(name string) {
+	f, _ := os.Open(name)
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		fmt.Println(s.Text())
+	}
+}
+
 func main() {
-	pc, _, _, _ := run\time.Caller(0)
+	pc, _, _, _ := runtime.Caller(0)
 	fmt.Println("start:" + runtime.FuncForPC(pc).Name())
 	fmt.Println("Hello, world")
 	time.Sleep(3 * time.Second) // 3秒だけ生かす
+
+	readFile("sample.txt")
+		
 	fmt.Println("end:" + runtime.FuncForPC(pc).Name())
 }
 
