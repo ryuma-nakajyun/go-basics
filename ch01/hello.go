440c66e6cfa197914c98f548408a3e684de0edec
ryuma-nakajyun <ryuma.nakajyun@outlook.com>
2026-07-18 20:11:09 +0900

    function start end log update

--- ch01/hello.go
+++ ch01/hello.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"time"
-	"runtime"
-	"os"
 	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"time"
 )
 
+// 定数宣言
+const TimeFormatMilli = "2006-01-02 15:04:05.000"
+
+// readFile
 func readFile(name string) {
+	pc, _, _, _ := runtime.Caller(0)
+	currentTime := time.Now()
+	fmt.Println("currentTime", currentTime.Format(TimeFormatMilli), "start:", runtime.FuncForPC(pc).Name())
+
 	f, _ := os.Open(name)
 	defer f.Close()
 
@@ -16,20 +24,22 @@ func readFile(name string) {
 	for s.Scan() {
 		fmt.Println(s.Text())
 	}
+
+	currentTime = time.Now()
+	fmt.Println("currentTime", currentTime.Format(TimeFormatMilli), "end:", runtime.FuncForPC(pc).Name())
 }
 
+// main
 func main() {
-	pc, _, _, _ := runtime.Caller(0)	
+	pc, _, _, _ := runtime.Caller(0)
 	currentTime := time.Now()
-	fmt.Println("currentTime", currentTime, "start:", runtime.FuncForPC(pc).Name())
+	fmt.Println("currentTime", currentTime.Format(TimeFormatMilli), "start:", runtime.FuncForPC(pc).Name())
 
 	fmt.Println("Hello, world")
 	time.Sleep(1 * time.Second) // 1秒だけ生かす
 
 	readFile("sample.txt")
-	
+
 	currentTime = time.Now()
-	fmt.Println("currentTime", currentTime, "end:", runtime.FuncForPC(pc).Name())
+	fmt.Println("currentTime", currentTime.Format(TimeFormatMilli), "end:", runtime.FuncForPC(pc).Name())
 }
-
-
