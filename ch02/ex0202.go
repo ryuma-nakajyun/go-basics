6662d6853c47192fa3312a0c19c1b8a744078bc0
ryuma-nakajyun <ryuma.nakajyun@outlook.com>
2026-07-18 20:35:49 +0900

    funciton start end log meassage

--- ch02/ex0202.go
+++ ch02/ex0202.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"runtime"
+	"time"
 )
 
+const TimeFormatMilli = "2006-01-02 15:04:05.000"
+
 func main() {
+	pc, _, _, _ := runtime.Caller(0)
+	currentTime := time.Now()
+	slog.Info("start", "time", currentTime.Format(TimeFormatMilli), "func", runtime.FuncForPC(pc).Name())
+
 	var x int = 10
 	var y float64 = 30.2
 	var z float64 = float64(x) + y
 	var d int = x + int(y)
 	fmt.Println(z, d)
+
+	currentTime = time.Now()
+	slog.Info("end", "time", currentTime.Format(TimeFormatMilli), "func", runtime.FuncForPC(pc).Name())
 }
