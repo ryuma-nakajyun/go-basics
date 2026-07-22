97b1950e23a64f441b469363a27e1970d982e120
ryuma-nakajyun <ryuma.nakajyun@outlook.com>
2026-07-23 05:34:29 +0900

    slog ver update

--- ch02/ex0202.go
+++ ch02/ex0202.go
@@ -3,21 +3,51 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"os"
 	"runtime"
 	"time"
 )
 
-func main() {
-	pc, _, _, _ := runtime.Caller(0)
-	currentTime := time.Now()
-	slog.Info("start", "time", currentTime.Format(TimeFormatMilli), "func", runtime.FuncForPC(pc).Name())
+const TimeFormatMilli = "2006-01-02 15:04:05.000"
 
+// slog の自動 timestamp を消すハンドラ
+func newHandler() slog.Handler {
+	return slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// slog が自動で付ける timestamp を削除
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+		AddSource: true, // file:line を出す
+	})
+}
+
+func main() {
+	logger := slog.New(newHandler())
+
+	pc, _, _, _ := runtime.Caller(0)
+	fn := runtime.FuncForPC(pc).Name()
+
+	// start
+	currentTime := time.Now()
+	logger.Info("start",
+		"time", currentTime.Format(TimeFormatMilli),
+		"func", fn,
+	)
+
+	// 処理
 	var x int = 10
 	var y float64 = 30.2
 	var z float64 = float64(x) + y
 	var d int = x + int(y)
 	fmt.Println(z, d)
 
+	// end
 	currentTime = time.Now()
-	slog.Info("end", "time", currentTime.Format(TimeFormatMilli), "func", runtime.FuncForPC(pc).Name())
+	logger.Info("end",
+		"time", currentTime.Format(TimeFormatMilli),
+		"func", fn,
+	)
 }
