822caae268ba0d4b295bf05c8409c1fb09d0c1de
ryuma-nakajyun <ryuma.nakajyun@outlook.com>
2026-07-27 17:16:36 +0900

    access columns by index

---
+++ orig01/ex0102.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"log/slog"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+const TimeFormatMilli = "2006-01-02 15:04:05.000"
+const r0711world = "r0711world_utf8.tsv"
+
+// slog の自動 timestamp を消すハンドラ
+func newHandler() slog.Handler {
+	return slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// slog が自動で付ける timestamp を削除
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+		AddSource: true, // file:line を出す
+	})
+}
+
+// readFile
+func readFile(name string) {
+	logger := slog.New(newHandler())
+	pc, _, _, _ := runtime.Caller(0)
+	fn := runtime.FuncForPC(pc).Name()
+
+	currentTime := time.Now()
+	logger.Info(fmt.Sprintf("%s start", currentTime.Format(TimeFormatMilli)), "func", fn)
+
+	f, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	reader := bufio.NewReader(f)
+	for {
+		line, isPrefix, err := reader.ReadLine() // isPrefix が true の場合は「1行が長すぎて分割されている」必要なら結合処理を書く
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if isPrefix {
+			log.Fatal("line too long")
+		}
+
+		fields := strings.Split(string(line), "\t")
+		fmt.Println(fields[0], fields[2], fields[3], fields[7], fields[8]) // fmt.Println(string(line[0])) これは先頭の一文字のみ
+
+	}
+
+	currentTime = time.Now()
+	logger.Info(fmt.Sprintf("%s end", currentTime.Format(TimeFormatMilli)), "func", fn)
+}
+
+// main
+func main() {
+	logger := slog.New(newHandler())
+	pc, _, _, _ := runtime.Caller(0)
+	fn := runtime.FuncForPC(pc).Name()
+
+	currentTime := time.Now()
+	logger.Info(fmt.Sprintf("%s start", currentTime.Format(TimeFormatMilli)), "func", fn)
+
+	readFile(r0711world)
+
+	currentTime = time.Now()
+	logger.Info(fmt.Sprintf("%s end", currentTime.Format(TimeFormatMilli)), "func", fn)
+}
